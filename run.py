59e8141ef593056a52dbb0c913a1d7e6601571a9
Dima <dimakazarin66@gmail.com>
2025-06-13 08:27:34 +0300

    1 commit

---
+++ run.py
@@ -0,0 +1,25 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.enums import ParseMode
+from aiohttp import web
+from app.handlers import router
+from webhook import setup_webhook_app
+
+TOKEN = '8190188550:AAE_Y4jwVLJqPlFh58BcB7Vg9KR05rJlO3Y'
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
